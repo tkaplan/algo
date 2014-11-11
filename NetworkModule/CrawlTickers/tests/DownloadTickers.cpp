1fbb8ebeb4f99a4a8974e123c9131363cb697542
Taylor Kaplan <taylor_kaplan@yahoo.com>
2014-11-10 19:14:52 -0800

    Fixed unit tests

--- NetworkModule/CrawlTickers/tests/DownloadTickers.cpp
+++ NetworkModule/CrawlTickers/tests/DownloadTickers.cpp
@@ -1,12 +1,16 @@
 #include <CrawlTickers.hpp>
-#define BOOST_TEST_MODULE MyTest
+#define BOOST_TEST_MODULE DownloadTickers
+#define BOOST_TEST_MAIN
+#define BOOST_TEST_DYN_LINK
 #include <boost/test/unit_test.hpp>
 
-//using namespace NetworkModule;
+BOOST_AUTO_TEST_SUITE ( DownloadTickers )
 
-BOOST_AUTO_TEST_CASE( my_test )
+BOOST_AUTO_TEST_CASE( get_all_tests )
 {
   NetworkModule::CrawlTickers* crawlTickers = new NetworkModule::CrawlTickers();
   int result = crawlTickers->downloadTickers();
   BOOST_REQUIRE(result == 0);
 }
+
+BOOST_AUTO_TEST_SUITE_END()
--- NetworkModule/CrawlTickers/tests/GetTickersForLetter.cpp
+++ NetworkModule/CrawlTickers/tests/GetTickersForLetter.cpp
@@ -1,11 +1,27 @@
 #include <CrawlTickers.hpp>
-#define BOOST_TEST_MODULE DownloadTickers
+#define BOOST_TEST_MODULE GetTickersForLetter
+#define BOOST_TEST_MAIN
+#define BOOST_TEST_DYN_LINK
 #include <boost/test/unit_test.hpp>
+
+#include <cpprest/http_client.h>
+#include <cpprest/filestream.h>
+
+using namespace utility;                    // Common utilities like string conversions
+using namespace web;                        // Common features like URIs.
+using namespace web::http;                  // Common HTTP functionality
+using namespace web::http::client;          // HTTP client features
+using namespace concurrency::streams;       // Asynchronous streams
+
 
 using namespace NetworkModule;
 
-BOOST_AUTO_TEST_CASE( DownloadTickers )
+BOOST_AUTO_TEST_SUITE ( GetTickersForLetter )
+
+BOOST_AUTO_TEST_CASE( test )
 {
+  std::shared_ptr<ostream> fileStream = std::make_shared<ostream>();
+
   CrawlTickers* crawlTickers = new CrawlTickers();
   int result;
   std::string file = "ticker";
@@ -15,3 +31,5 @@ BOOST_AUTO_TEST_CASE( DownloadTickers )
   result = crawlTickers->getTickersForLetter(fileStream, file, letter);
   BOOST_REQUIRE(result == 0);
 }
+
+BOOST_AUTO_TEST_SUITE_END()
